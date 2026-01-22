11d6a1a135c18b8d1403814350374fbdd518c442
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-01-22 11:51:37 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from peewee import *
+
+db = SqliteDatabase('sistema_vendas.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Produto(BaseModel):
+    nome = CharField()
+    preco = DecimalField(decimal_places=2)
+    estoque = IntegerField()
+
+class Venda(BaseModel):
+    produto = ForeignKeyField(Produto, backref='vendas')
+    quantidade = IntegerField()
+    data_venda = DateTimeField(constraints=[SQL('DEFAULT CURRENT_TIMESTAMP')])
+
+
+db.connect()
+db.create_tables([Produto, Venda])
